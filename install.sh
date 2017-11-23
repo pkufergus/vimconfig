7028d0831fa3ceb421a18bb8cdda0b84968a3a12
Cao Song <caosong@baidu.com>
2017-11-23 14:43:27 +0800

    fix cp bug and set env
    Signed-off-by: Cao Song <caosong@baidu.com>

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ fi
 tar -zxvf vim.tar.gz
 mv .vim $HOME/
 
-cp $HOME/.vimrc $HOME/.vimrc.bak
+if [ -f $HOME/.vimrc ]; then
+    cp $HOME/.vimrc $HOME/.vimrc.bak
+if
 echo "source $HOME/.vim/vimrc" > ~/.vimrc
+echo "export LANG=en_US.UTF-8" >> ~/.bash_profile
+echo "export TERM=xterm-256color" >> ~/.bash_profile
